f3a01e57c890952a6be2bb31cca19272ebf119f7
chunchuprati <prathibha.panga@gmail.com>
2017-11-27 21:59:40 -0800

    Update flashcard.js

--- flashcard.js
+++ flashcard.js
@@ -4,16 +4,16 @@ var  fs = require("fs");
 var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
 
-// var firstPresident = BasicCard("Who is the first President of United States?", "George Washington");
-// "Who was the first President of United States"
-// console.log(firstPresident.front.blue);
+var firstPresident = BasicCard("Who is the first President of United States?", "George Washington");
+"Who was the first President of United States"
+console.log(firstPresident.front.blue);
 
-// "George Washington"
-// console.log(firstPresident.back.yellow)
+"George Washington"
+console.log(firstPresident.back.yellow)
 
-// var firstPresidentCloze =  ClozeCard("George Washington was the first President of United States", "first President");
-// console.log(firstPresidentCloze.cloze.rainbow);
-// console.log(firstPresidentCloze.partial.magenta);
-// console.log(firstPresidentCloze.fullText.cyan);
+var firstPresidentCloze =  ClozeCard("George Washington was the first President of United States", "first President");
+console.log(firstPresidentCloze.cloze.rainbow);
+console.log(firstPresidentCloze.partial.magenta);
+console.log(firstPresidentCloze.fullText.cyan);
 
-// var brokenCloze = new ClozeCard("This doesn't work", "oops");
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
